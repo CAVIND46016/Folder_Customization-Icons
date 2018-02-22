52d6149b666f4de0e43785117a676c8306a2db15
Cavin Dsouza <cavin46016@gmail.com>
2018-02-21 21:32:29 -0500

    Improvement
    Added recursive functionality, will check all directories while applying icons.

--- icon_apply_GUI.py
+++ icon_apply_GUI.py
@@ -3,6 +3,7 @@ from tkinter import ttk
 from tkinter import messagebox
 from tkinter.filedialog import askdirectory
 from IconApply import seticon
+from os.path import isfile, join
 import os
 
 class CreateGUI():
@@ -12,24 +13,18 @@ class CreateGUI():
         self.root_obj = root_obj;
         self.construct_dialog()
         
-    def set_icon(self, directory):
-        subdirs = []
-        for filename in os.listdir(directory):
-            a = os.path.join(directory, filename)
-            if(os.path.isdir(a)):
-                subdirs.append(a)
-                
-        for i in subdirs:
-            for filename in os.listdir(i):     
-                icofile = None;
-                for ico_file in os.listdir(i):
-                    if(ico_file.endswith(".ico")):
-                        icofile = i + "\\" + ico_file
-                        break;
-                    
-                if(icofile != None):   
-                    seticon(i + "\\", icofile, 0)
+    def bulk_icon_apply(self, directory):
+        main_directory_icon = self.get_ico_file(directory);
+        if(main_directory_icon):
+            seticon(directory, main_directory_icon, 0)
         
+        for root, dirs, files in os.walk(directory):
+            for name in dirs:
+                subdir = os.path.join(root, name)
+                subdir_icon = self.get_ico_file(subdir);
+                if(subdir_icon):
+                    seticon(subdir, subdir_icon, 0)
+
         messagebox.showinfo("Success","Icons applied successfully.")
         
     def get_directory(self):
@@ -37,28 +32,35 @@ class CreateGUI():
         if(self.dir_name):
             if(os.path.isdir(self.dir_name)):
                 try:
-                    self.set_icon(self.dir_name)
+                    self.bulk_icon_apply(self.dir_name)
                 except IOError as e:
                     messagebox.showerror("Error", e)
             else:
                 messagebox.showerror("Error", "Invalid directory.")
+                
+    def get_ico_file(self, directory):
+        for f in os.listdir(directory):
+            f_path = join(directory, f)
+            if(isfile(f_path) and f.endswith(".ico")):
+                return f_path
+            
+        return None;
         
     def construct_dialog(self):
-        self.root_obj.title("Batch Folder Icon Changer")
-        self.root_obj.geometry('{}x{}'.format(150, 50))
-        self.root_obj.resizable(width=False, height=False)
-        label = ttk.Label(self.root_obj, text ="Batch Icon Changer",font=("Arial", 10))
+        self.root_obj.title("Batch Icon Changer")
+        self.root_obj.geometry('{}x{}'.format(175, 60))
+        
+        label = ttk.Label(self.root_obj, text ="Batch Icon Changer", font=("Arial", 10))
         label.pack()
         
         button = ttk.Button(self.root_obj, text = 'Select directory', command = self.get_directory);
         button.pack()
-        
-        self.root_obj.mainloop()
 
 
 def main():
     root = Tk()
     CreateGUI(root);
+    root.mainloop()
     
 if(__name__ == "__main__"):
     main();
