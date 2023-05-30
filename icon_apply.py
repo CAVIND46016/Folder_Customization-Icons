de7ed8a3858d094ecd2239401e77fd91f9281396
Cavin Dsouza <21344198+CAVIND46016@users.noreply.github.com>
2023-05-30 02:37:32 -0400

    Update icon_apply.py

--- icon_apply.py
+++ icon_apply.py
@@ -1,42 +1,44 @@
 """
 Contains logic of applying icon to a folder.
+Title: How to change folder icons with Python on windows?
+Author: cgohlke
+Date: 01/12/2011
+URL: https://stackoverflow.com/questions/4662759/how-to-change-folder-icons-with-python-on-windows
 """
+
 import os
 import ctypes
 from ctypes import POINTER, Structure, c_wchar, c_int, sizeof, byref
 from ctypes.wintypes import BYTE, WORD, DWORD, LPWSTR
 import win32api
 
-#************************************************************
-#* Title: How to change folder icons with Python on windows?
-#* Author: cgohlke
-#* Date: 01/12/2011
-#* URL: https://stackoverflow.com/questions/4662759/how-to
-#       -change-folder-icons-with-python-on-windows
-#************************************************************
 HICON = c_int
 LPTSTR = LPWSTR
 TCHAR = c_wchar
 MAX_PATH = 260
-FCSM_ICONFILE = 0x00000010
-FCS_FORCEWRITE = 0x00000002
-SHGFI_ICONLOCATION = 0x000001000    
+FCSM_ICON_FILE = 0x00000010
+FCS_FORCE_WRITE = 0x00000002
+SHGFI_ICON_LOCATION = 0x000001000
 
-# https://docs.python.org/3/library/ctypes.html#structures-and-unions
+
 class GUID(Structure):
     """
     Structure definition.
+    See https://docs.python.org/3/library/ctypes.html#structures-and-unions
     """
+
     _fields_ = [
         ('Data1', DWORD),
         ('Data2', WORD),
         ('Data3', WORD),
         ('Data4', BYTE * 8)]
 
-class SHFOLDERCUSTOMSETTINGS(Structure):
+
+class SHFolderCustomSettings(Structure):
     """
     Structure definition.
     """
+
     _fields_ = [
         ('dw_size', DWORD),
         ('dw_mask', DWORD),
@@ -54,47 +56,71 @@ class SHFOLDERCUSTOMSETTINGS(Structure):
         ('pszLogo', LPTSTR),
         ('cchLogo', DWORD)]
 
-class SHFILEINFO(Structure):
+
+class SHFileInfo(Structure):
     """
     Structure definition.
     """
+
     _fields_ = [
         ('hIcon', HICON),
         ('iIcon', c_int),
         ('dwAttributes', DWORD),
         ('szDisplayName', TCHAR * MAX_PATH),
-        ('szTypeName', TCHAR * 80)]    
+        ('szTypeName', TCHAR * 80)]
 
-def seticon(folderpath, iconpath, iconindex):
+
+def set_icon(folder_path, icon_path, icon_index):
     """
-    Applies 'iconpath' to 'folderpath'
+    Applies 'icon_path' to 'folder_path'
+    :param folder_path:
+    :param icon_path:
+    :param icon_index:
+    :return:
     """
+
     shell32 = ctypes.windll.shell32
 
-    folderpath = str(os.path.abspath(folderpath))
-    iconpath = str(os.path.abspath(iconpath))
+    folder_path = str(os.path.abspath(folder_path))
+    icon_path = str(os.path.abspath(icon_path))
 
-    fcs = SHFOLDERCUSTOMSETTINGS()
+    fcs = SHFolderCustomSettings()
     fcs.dw_size = sizeof(fcs)
-    fcs.dw_mask = FCSM_ICONFILE
-    fcs.psz_icon_file = iconpath
+    fcs.dw_mask = FCSM_ICON_FILE
+    fcs.psz_icon_file = icon_path
     fcs.cch_icon_file = 0
-    fcs.i_icon_index = iconindex
+    fcs.i_icon_index = icon_index
 
-    gs_fcs = shell32.SHGetSetFolderCustomSettings(byref(fcs), \
-                                                  folderpath, FCS_FORCEWRITE)
+    gs_fcs = shell32.SHGetSetFolderCustomSettings(
+        byref(fcs),
+        folder_path,
+        FCS_FORCE_WRITE
+    )
     if gs_fcs:
         raise WindowsError(win32api.FormatMessage(gs_fcs))
 
-    sfi = SHFILEINFO()
-    gs_fcs = shell32.SHGetFileInfoW(folderpath, 0, byref(sfi), \
-                                    sizeof(sfi), SHGFI_ICONLOCATION)
+    sfi = SHFileInfo()
+    gs_fcs = shell32.SHGetFileInfoW(
+        folder_path,
+        0,
+        byref(sfi),
+        sizeof(sfi),
+        SHGFI_ICON_LOCATION
+    )
     if gs_fcs == 0:
         raise WindowsError(win32api.FormatMessage(gs_fcs))
 
-    index = shell32.Shell_GetCachedImageIndexW(sfi.szDisplayName, sfi.iIcon, 0)
+    index = shell32.Shell_GetCachedImageIndexW(
+        sfi.szDisplayName,
+        sfi.iIcon,
+        0
+    )
     if index == -1:
         raise WindowsError()
 
-    shell32.SHUpdateImageW(sfi.szDisplayName, sfi.iIcon, 0, index)
-    
+    shell32.SHUpdateImageW(
+        sfi.szDisplayName,
+        sfi.iIcon,
+        0,
+        index
+    )
